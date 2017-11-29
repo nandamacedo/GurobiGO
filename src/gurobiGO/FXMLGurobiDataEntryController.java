211e7fb5b9abf111348f0255e86a2bafae218e18
Fernanda Macedo <nanda.macedo.san@gmail.com>
2017-11-29 01:03:46 -0200

    Novos arquivos com novas funcionalidades.

--- src/gurobiGO/FXMLGurobiDataEntryController.java
+++ src/gurobiGO/FXMLGurobiDataEntryController.java
@@ -5,35 +5,60 @@
  */
 package gurobiGO;
 
-import gurobi.*;
-
+import gurobi.GRB;
+import gurobi.GRBEnv;
+import gurobi.GRBException;
+import gurobi.GRBLinExpr;
+import gurobi.GRBModel;
+import gurobi.GRBVar;
 import java.net.URL;
-import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import sun.security.pkcs.ParsingException;
 
 /**
  * FXML Controller class
  *
  * @author nanda
  */
-public class FXMLGurobiController implements Initializable {
+public class FXMLGurobiDataEntryController implements Initializable {
 
     @FXML
     private Label labelSolve;
+    @FXML
+    private Label labelT;
+    @FXML
+    private Label labelNomeDpto;
     @FXML
     private TextField txtDepto;
     @FXML
     private Button buttonOk;
     @FXML
     private Pane pane;
+    @FXML
+    private TextField rt1;
+    @FXML
+    private TextField rt2;
+    @FXML
+    private TextField rt3;
+    @FXML
+    private TextField rt4;
+    @FXML
+    private TextField rt5;
+    @FXML
+    private TextField rt6;
+    @FXML
+    private TextField rt7;
+    @FXML
+    private TextField rt8;
+    @FXML
+    private TextField rt9;
     @FXML
     private Label h9;
     @FXML
@@ -52,6 +77,9 @@ public class FXMLGurobiController implements Initializable {
     private Label h16;
     @FXML
     private Label h17;
+    
+
+    private Double rt;
 
     @FXML
     private void GurobiGo() {
@@ -62,41 +90,45 @@ public class FXMLGurobiController implements Initializable {
             GRBModel model = new GRBModel(env);
 
             // Create variables
-            GRBVar x1 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x1");
-            GRBVar x2 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x2");
-            GRBVar x3 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x3");
-            GRBVar x4 = model.addVar(0.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x4");
-            GRBVar x5 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x5");
-            GRBVar x6 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x6");
-            GRBVar x7 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x7");
-            GRBVar x8 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x8");
-            GRBVar x9 = model.addVar(1.0, GRB.INFINITY, 0.0, GRB.CONTINUOUS, "x9");
+            GRBVar x1 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x1");
+            GRBVar x2 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x2");
+            GRBVar x3 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x3");
+            GRBVar x4 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x4");
+            GRBVar x5 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x5");
+            GRBVar x6 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x6");
+            GRBVar x7 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x7");
+            GRBVar x8 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x8");
+            GRBVar x9 = model.addVar(0.0, GRB.INFINITY, 1.0, GRB.INTEGER, "x9");
 
             // Set objective: minimize x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
             GRBLinExpr expr = new GRBLinExpr();
-            expr.addTerm(1.0, x1);
+            /* expr.addTerm(1.0, x1);
             expr.addTerm(1.0, x2);
             expr.addTerm(1.0, x3);
-            expr.addTerm(0.0, x4);
+            expr.addTerm(1.0, x4);
             expr.addTerm(1.0, x5);
             expr.addTerm(1.0, x6);
             expr.addTerm(1.0, x7);
             expr.addTerm(1.0, x8);
-            expr.addTerm(1.0, x9);
-            model.setObjective(expr, GRB.MINIMIZE);
+            expr.addTerm(1.0, x9);*/
+            // model.setObjective(expr, GRB.MINIMIZE);*/
+            model.set(GRB.IntAttr.ModelSense, GRB.MINIMIZE);
 
             // Add constraint: x1 >= 2
+            rt = Double.parseDouble(rt1.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x1);
-            model.addConstr(expr, GRB.GREATER_EQUAL, 2.0, "c1");
+            model.addConstr(expr, GRB.GREATER_EQUAL, rt, "c1");
 
             // Add constraint: x1 + x2 >= 2
+            rt = Double.parseDouble(rt2.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x1);
             expr.addTerm(1.0, x2);
             model.addConstr(expr, GRB.GREATER_EQUAL, 2.0, "c2");
 
             // Add constraint: x1 + x2 +x3 >= 3
+            rt = Double.parseDouble(rt3.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x1);
             expr.addTerm(1.0, x2);
@@ -104,29 +136,33 @@ public class FXMLGurobiController implements Initializable {
             model.addConstr(expr, GRB.GREATER_EQUAL, 3.0, "c3");
 
             // Add constraint:  x2 + x3 >= 3
+            rt = Double.parseDouble(rt4.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x3);
             expr.addTerm(1.0, x2);
             model.addConstr(expr, GRB.GREATER_EQUAL, 3.0, "c4");
 
-            // Add constraint: x4 = 0
+            // Add constraint: x4 = 0            
             expr = new GRBLinExpr();
             expr.addTerm(0.0, x4);
             model.addConstr(expr, GRB.EQUAL, 0.0, "c5");
 
             // Add constraint:  x5 +x3 >= 3
+            rt = Double.parseDouble(rt5.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x5);
             expr.addTerm(1.0, x3);
             model.addConstr(expr, GRB.GREATER_EQUAL, 3.0, "c6");
 
             // Add constraint:  x6 + x5 >= 3
+            rt = Double.parseDouble(rt6.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x6);
             expr.addTerm(1.0, x5);
             model.addConstr(expr, GRB.GREATER_EQUAL, 3.0, "c7");
 
             // Add constraint:  x7 +x6 +x5 >= 3
+            rt = Double.parseDouble(rt7.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x7);
             expr.addTerm(1.0, x6);
@@ -134,6 +170,7 @@ public class FXMLGurobiController implements Initializable {
             model.addConstr(expr, GRB.GREATER_EQUAL, 3.0, "c8");
 
             // Add constraint:  x8 +x7 +x6 >= 3
+            rt = Double.parseDouble(rt8.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x8);
             expr.addTerm(1.0, x7);
@@ -141,6 +178,7 @@ public class FXMLGurobiController implements Initializable {
             model.addConstr(expr, GRB.GREATER_EQUAL, 3.0, "c9");
 
             // Add constraint:  x9 +x8 +x7 >= 3
+            rt = Double.parseDouble(rt9.getText());
             expr = new GRBLinExpr();
             expr.addTerm(1.0, x9);
             expr.addTerm(1.0, x8);
@@ -176,7 +214,7 @@ public class FXMLGurobiController implements Initializable {
             String label;
 
             label = Double.toString(model.get(GRB.DoubleAttr.ObjVal));
-            labelSolve.setText("O " + txtDepto.getText() + " precisa de no mínimo: " + label.substring(0, label.length() - 2) + " estagiários.");
+            labelT.setText("O " + txtDepto.getText() + " precisa de no mínimo: " + label.substring(0, label.length() - 2) + " estagiários.");
 
             // Dispose of model and environment
             model.dispose();
@@ -191,9 +229,31 @@ public class FXMLGurobiController implements Initializable {
     @FXML
     private void handleButtonAction(ActionEvent event) {
         if (!txtDepto.getText().equals("")) {
-            GurobiGo();
-            pane.setVisible(true);
-            labelSolve.setVisible(true);
+            GurobiGo(); 
+            labelNomeDpto.setText(txtDepto.getText());
+            txtDepto.setVisible(false);
+            labelNomeDpto.setVisible(true);
+            labelT.setVisible(true);
+            labelSolve.setText("Número de Estagiários Iniciando no Turno:");
+            rt1.setVisible(false);
+            h9.setVisible(true);
+            rt2.setVisible(false);
+            h10.setVisible(true);
+            rt3.setVisible(false);
+            h11.setVisible(true);
+            rt4.setVisible(false);
+            h12.setVisible(true);
+            rt5.setVisible(false);
+            h13.setVisible(true);
+            rt6.setVisible(false);
+            h14.setVisible(true);
+            rt7.setVisible(false);
+            h15.setVisible(true);
+            rt8.setVisible(false);
+            h16.setVisible(true);
+            rt9.setVisible(false);
+            h17.setVisible(true);
+            buttonOk.setVisible(false);
         } else {
 
         }
@@ -202,16 +262,46 @@ public class FXMLGurobiController implements Initializable {
     @FXML
     private void handleButtonLimpar(ActionEvent event
     ) {
-        txtDepto.clear();
-        labelSolve.setVisible(false);
-        pane.setVisible(false);
+            
+            txtDepto.setVisible(true);
+            txtDepto.clear();
+            labelNomeDpto.setVisible(false);
+            labelT.setVisible(false);
+            labelSolve.setText("Nº Mínimo de Estagiários:");
+            rt1.setVisible(true);
+            rt1.clear();
+            h9.setVisible(false);
+            rt2.setVisible(true);
+            rt2.clear();
+            h10.setVisible(false);
+            rt3.setVisible(true);
+            rt3.clear();
+            h11.setVisible(false);
+            rt4.setVisible(true);
+            rt4.clear();
+            h12.setVisible(false);
+            rt5.setVisible(true);
+            rt5.clear();
+            h13.setVisible(false);
+            rt6.setVisible(true);
+            rt6.clear();
+            h14.setVisible(false);
+            rt7.setVisible(true);
+            rt7.clear();
+            h15.setVisible(false);
+            rt8.setVisible(true);
+            rt8.clear();
+            h16.setVisible(false);
+            rt9.setVisible(true);
+            rt9.clear();
+            h17.setVisible(false);
+            buttonOk.setVisible(true);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb
     ) {
-        pane.setVisible(false);
+        pane.setVisible(true);
 
     }
-
 }
